8cea8d3fe51aa7d82b625f7d8a0eb0934248a544
asrafulashiq <asrafulashiq@gmail.com>
2014-06-15 12:46:47 +0600

    pairing and discovering

--- gen/com/example/tictack/R.java
+++ gen/com/example/tictack/R.java
@@ -36,24 +36,27 @@ public final class R {
         public static final int logo2=0x7f020009;
     }
     public static final class id {
-        public static final int about_game=0x7f09000e;
-        public static final int action_settings=0x7f090011;
-        public static final int button1=0x7f090002;
-        public static final int button2=0x7f090003;
-        public static final int button3=0x7f090004;
-        public static final int button4=0x7f090005;
-        public static final int button5=0x7f090006;
-        public static final int button6=0x7f090007;
-        public static final int button7=0x7f090008;
-        public static final int button8=0x7f090009;
-        public static final int button9=0x7f09000a;
-        public static final int continue_game=0x7f09000d;
-        public static final int imageView1=0x7f090001;
-        public static final int listView1=0x7f090000;
-        public static final int multi_player=0x7f09000c;
-        public static final int multi_player_radio=0x7f090010;
-        public static final int new_game=0x7f09000b;
-        public static final int single_player_radio=0x7f09000f;
+        public static final int about_game=0x7f090011;
+        public static final int action_settings=0x7f090014;
+        public static final int button1=0x7f090005;
+        public static final int button2=0x7f090006;
+        public static final int button3=0x7f090007;
+        public static final int button4=0x7f090008;
+        public static final int button5=0x7f090009;
+        public static final int button6=0x7f09000a;
+        public static final int button7=0x7f09000b;
+        public static final int button8=0x7f09000c;
+        public static final int button9=0x7f09000d;
+        public static final int continue_game=0x7f090010;
+        public static final int discover=0x7f090000;
+        public static final int discoveredDevices=0x7f090001;
+        public static final int imageView1=0x7f090004;
+        public static final int multi_player=0x7f09000f;
+        public static final int multi_player_radio=0x7f090013;
+        public static final int new_game=0x7f09000e;
+        public static final int pair=0x7f090002;
+        public static final int pairedDevices=0x7f090003;
+        public static final int single_player_radio=0x7f090012;
     }
     public static final class layout {
         public static final int activity_bluetooth_connect=0x7f030000;
@@ -71,9 +74,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int continue_game=0x7f050004;
+        public static final int discover=0x7f05000a;
         public static final int hello_world=0x7f050002;
         public static final int multi_player=0x7f050008;
         public static final int new_game=0x7f050003;
+        public static final int pair=0x7f05000b;
         public static final int single_player=0x7f050007;
         public static final int title_activity_bluetooth_connect=0x7f050009;
         public static final int title_activity_game=0x7f050006;
